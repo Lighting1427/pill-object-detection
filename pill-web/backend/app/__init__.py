774d84ed31fb668d59fea9c215d6ea37733c0cf9
satang <satang1427@gmail.com>
2026-09-15 10:21:28 +0700

    delete not use file

--- pill-web/backend/app/__init__.py
+++
@@ -1,2 +0,0 @@
-"""PillVision backend package."""
-
--- pill-web/frontend/run_frontend.ps1
+++
@@ -1,5 +0,0 @@
-$ErrorActionPreference = "Stop"
-$FrontendDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location -LiteralPath $FrontendDirectory
-& npm.cmd run dev
-
